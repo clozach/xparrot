a07067298c89d0f8556f214b8de6fda57927f44a
Chris Lozac'h <clozach@gracenote.com>
2018-03-04 22:33:46 -0800

    Slightly more flexible functions
    Note: no idea how close this is to canonical Python. It's not my first, or even fourth language.

--- xparrot.py
+++ xparrot.py
@@ -1,5 +1,7 @@
 import sys
 import requests
+import json
+from pprint import pprint
 
 if len(sys.argv) != 2:
   sys.exit("Usage: `python` " + sys.argv[0] + " <AIRTABLE_API_KEY>")
@@ -11,15 +13,37 @@ def fetch_stale_tasks(api_key):
     headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + api_key}
     theFilter = "AND({Status}='',DATETIME_DIFF(TODAY(),{CreationTime},'days')>7)"
     data = {'filterByFormula':theFilter}
-    r1 = requests.get(tasks_table_root, params=data, headers=headers)
-    print(r1)
 
-def auto_archive_task(task_id, api_key):
+    response = requests.get(tasks_table_root, params=data, headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.content)['records']
+    else:
+        print('No records found: [' + str(response.status_code) + ']:' + response.reason)
+
+def auto_archive_task(task, api_key):
     data = {'fields': {'Status': 'Auto-archived'}}
     headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer ' + airtable_api_key}
-    r = requests.patch(tasks_table_root + '/' + task_id, json=data, headers=headers)
 
-    print(r)
+    response = requests.patch(tasks_table_root + '/' + task['id'], json=data, headers=headers)
 
-fetch_stale_tasks(airtable_api_key)
-auto_archive_task('recMPxChvmkXCWYJG', airtable_api_key)
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.content)
+    else:
+        print('Auto-archive failed for task ' + task['id'] + '(' + task['Name'] + '): [' + str(response.status_code) + ']:' + response.reason)
+
+def auto_archive_stale_tasks():
+    tasks = fetch_stale_tasks(airtable_api_key)
+    if tasks is not None:
+        print("\n🐕 Fetched these stale tasks 🐕")
+        pprint(tasks)
+        print("🐕 ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 🐕\n")
+
+        for task in tasks:
+            auto_archive_response = auto_archive_task(task, airtable_api_key)
+            if auto_archive_response is None:
+                print("😭 Dang.")
+            else:
+                print('Auto-archived 👉  ' + auto_archive_response['fields']['Name'])
+
+auto_archive_stale_tasks()
