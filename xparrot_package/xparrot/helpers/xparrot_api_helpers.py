ff032a6f1811f81378a717c60b8b2592f43b223c
Chris Lozac'h <clozach@gracenote.com>
2018-03-30 23:08:06 -0700

    Fancy styles using terminal colors/codes
    https://stackoverflow.com/questions/287871/print-in-terminal-with-colors (@joeld's answer)

--- xparrot_package/xparrot/helpers/xparrot_api_helpers.py
+++ xparrot_package/xparrot/helpers/xparrot_api_helpers.py
@@ -8,13 +8,37 @@ from typing import Generator, Iterator
 def print_tasks(self, tasksResponse: Generator[Iterator, None, None]):
     tasks = next(tasksResponse)
     if (tasks is None) or (len(tasks) == 0):
-        print("No tasks to auto-archive. 😎")
+        print(C.WARNING + "No tasks to auto-archive. 😎" + C.ENDC)
         return [], []
     else:
         for task in tasks:
             print('\n〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰')
             # print("\n", json.dumps(task['fields'], indent=4), "\n")
             for k, v in task['fields'].items():
-                if k == 'Notes': continue
-                print(k.upper(), ": ", v)
+                keyStyle = C.HEADER
+                valStyle = ''
+                if k == 'Notes':
+                    continue
+                elif k == 'Status':
+                    valStyle = C.OKGREEN
+                    if v == 'Auto-archived':
+                        valStyle = C.UNDERLINE + C.OKBLUE
+                elif k == 'Name':
+                    valStyle = C.BOLD
+                else:
+                    print(C.HEADER + k.title() + C.ENDC, ": ", v)
+                    continue
+
+                print(C.HEADER + k.title() + C.ENDC, ": ", valStyle + v + C.ENDC)
+
             print('〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰〰\n')
+
+class C:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
