bb326d59c5ea38d54107aa0d0e948e77ef059237
Chris Lozac'h <clozach@gracenote.com>
2018-03-31 00:00:57 -0700

    Rainbow asciimatics sample

---
+++ components_cookbook/rainbow.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from asciimatics.effects import RandomNoise  # $ pip install asciimatics
+from asciimatics.renderers import SpeechBubble, Rainbow
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError
+
+
+def demo(screen):
+    render = Rainbow(screen, SpeechBubble('              \n              \n    This is   \n  a rainbow   \n              \n              '))
+    effects = [RandomNoise(screen, signal=render)]
+    screen.play([Scene(effects, -1)], stop_on_resize=True)
+
+while True:
+    try:
+        Screen.wrapper(demo)
+        break
+    except ResizeScreenError:
+        pass
